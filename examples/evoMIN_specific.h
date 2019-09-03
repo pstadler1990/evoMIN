af01f926886a55ff98cdde12a2b74a92213ee1d0
PStadler <pstadler90@gmx.de>
2019-09-03 10:21:12 +0200

    Add files via upload

---
+++ examples/evoMIN_specific.h
@@ -0,0 +1,15 @@
+#ifndef EVOMIN_SPECIFIC_H
+#define EVOMIN_SPECIFIC_H
+
+#define EVOMIN_MAX_PAYLOAD_SIZE			(uint32_t)50
+#define EVOMIN_P_BUF_SIZE				(uint32_t)EVOMIN_MAX_PAYLOAD_SIZE
+#define EVOMIN_BUFFER_SIZE				(uint32_t)EVOMIN_P_BUF_SIZE + 2
+#define	EVOMIN_SEND_RETRIES_ON_FAIL		(uint32_t)3
+#define EVOMIN_SEND_RETRY_MIN_TIME		(uint32_t)3	// TODO: Replace with suitable value (depending on communication interface speed etc.)
+
+enum evoMin_Command {
+	EVOMIN_CMD_RESERVED = 0x00,
+	EVOMIN_CMD_SEND_IDN = 0xCD
+};
+
+#endif
---
+++ examples/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "evomin.h"
+#include "evoMIN_tx_impl.h"
+#include "testdata.h"
+
+struct evoMin_Interface comInterface;
+
+static int8_t MIN_Test_ForceSendFrame(void);
+
+int main() {
+	evoMin_Init(&comInterface);
+	evoMin_SetTXHandler(&comInterface, &evoMin_Handler_TX);
+
+	MIN_Test_ForceSendFrame();
+
+	return 0;
+}
+
+
+int8_t
+MIN_Test_ForceSendFrame(void)  {
+	struct evoMin_Frame sendFrame;
+
+	/* Initialize payload */
+	uint8_t buf[] = {
+			0xDE,
+			0xAD,
+			0xBE,
+			0xEF
+	};
+
+	/* Create and queue frame to be sent over the low-level interface */
+	evoMin_CreateFrame(&sendFrame, EVOMIN_CMD_SEND_IDN, buf, 4);
+	evoMin_QueueFrame(&comInterface, sendFrame);
+
+	/* This function forces a frame to be sent immediately */
+	evoMin_SendFrameImmediately(&comInterface, sendFrame);
+
+	/* Otherwise, we could call the function below in our main loop */
+	// evoMin_SendResendLastFrame(&comInterface);
+
+	return 1;
+}
+
