9cce701d8db530d4d0c961a02e755f7a7598bf7c
PStadler <pstadler90@gmx.de>
2019-09-03 10:23:44 +0200

    Delete evoMIN_tx_impl.h

--- examples/evoMIN_tx_impl.h
+++
@@ -1,9 +0,0 @@
-#ifndef EVOMIN_TX_IMPL_H
-#define EVOMIN_TX_IMPL_H
-
-uint8_t evoMin_CRC8(uint8_t* bytes, uint32_t bLen);
-uint8_t evoMin_Handler_TX(uint8_t byte);
-uint32_t evoMin_GetTimeNow(void);
-void evoMin_Handler_FrameRecvd(struct evoMin_Frame* frame);
-
-#endif //EVOMIN_TX_IMPL_H
